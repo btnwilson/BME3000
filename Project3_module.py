f5e806be241469816b302147cd46d73d0f5ce569
Abbyhow <153241032+Abbyhow@users.noreply.github.com>
2023-12-09 15:43:41 -0500

    project3_module.py

--- Project3_module.py
+++ Project3_module.py
@@ -29,22 +29,28 @@ def remove_peaks(data_dict, fs, threshold):
         
     return no_peak_data
 
-def plot_data(signal, fs,title='Title', units='A.U.'):
+def plot_data(signal, fs,title = None, units ='A.U.', label = None):
     time = np.arange(0,len(signal)/fs, 1/fs)
-    plt.plot(time, signal)
+    plt.plot(time, signal, label = label)
     plt.title(title)
     plt.xlabel('time (s)')
     plt.ylabel(units)
     plt.grid()
+    plt.legend()
+    plt.title(title)
+         
   
-def plot_filtered_data(signal, impulse_response, fs, title = 'Title', units = 'A.U.'):
+def filter_data(signal, impulse_response):
     filtered_signal = np.convolve(signal, impulse_response, mode = 'same')
-    plot_data(filtered_signal, fs, title = title, units = units )
+    
     return filtered_signal
 
-def find_beats(signal, fs):
-    flipped_signal = signal * -1
-    beats, info = scipy.signal.find_peaks(flipped_signal, height = 100)
+def find_beats(signal, fs, flipped = False):
+    if flipped == True:
+        processed_signal = signal * -1
+    else:
+        processed_signal = signal
+    beats, info = scipy.signal.find_peaks(processed_signal, height = 100)
     time = np.arange(0,len(signal)/fs, 1/fs)
     plt.scatter(time[beats], signal[beats], c = 'r')
     return beats
@@ -56,7 +62,7 @@ def hrv(beat_indicies, fs):
     hrv = np.std(differences)
     return differences, hrv
 
-def plot_frequency_bands(signal, fs, low_fc_range, high_fc_range, title = 'Title', units = 'A.U'):
+def plot_frequency_bands(signal, fs, low_fc_range, high_fc_range, title = None, units = 'A.U.'):
     fft = scipy.fft.rfft(signal)
     freq = scipy.fft.rfftfreq(len(signal), fs)
     
@@ -69,13 +75,14 @@ def plot_frequency_bands(signal, fs, low_fc_range, high_fc_range, title = 'Title
     high_fc_fft = fft[high_fc_mask]
     high_fc = np.arange(high_fc_range[0], high_fc_range[1], ((high_fc_range[1] - high_fc_range[0]) / len(high_fc_fft)))
    
-    fft_power = ((np.abs(fft)) ** 2) #/ ((len(signal) * fs))
+    fft_power = ((np.abs(fft)) ** 2) / ((len(signal) * fs))
     
     plt.plot(freq, fft_power, c = 'gray', zorder = 0 )
     plt.fill_between(low_fc, np.abs(fft_power[low_fc_mask]), label = 'low frequecy band')
     plt.fill_between(high_fc, np.abs(fft_power[high_fc_mask]), label = 'high frequency band')
     
     plt.title(title)
+    plt.ylabel(units)
     plt.xlabel('frequencies')
     plt.xlim(0,high_fc_range[1])
     plt.legend()
@@ -86,6 +93,21 @@ def plot_frequency_bands(signal, fs, low_fc_range, high_fc_range, title = 'Title
     
     return ratio
     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 
