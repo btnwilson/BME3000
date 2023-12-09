93807ce421e92590d5a04540082898b6904a372b
Abbyhow <153241032+Abbyhow@users.noreply.github.com>
2023-12-09 13:08:53 -0500

    Add files via upload

---
+++ Project3_module.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  4 13:53:20 2023
+
+@author: abbyhoward
+
+project3_module.py
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy
+
+#%% part 1
+
+def remove_peaks(data_dict, fs, threshold):
+    no_peak_data = {}
+    for key in data_dict:
+        signal = data_dict[key]
+        
+        indicies = np.arange(0,len(signal))
+        
+        peaks = indicies[signal > threshold]
+        
+        for index in peaks:
+            signal[index] = (signal[index + 1] + signal[index - 1]) / 2
+       
+        no_peak_data = no_peak_data | {key : signal}
+        
+    return no_peak_data
+
+def plot_data(signal, fs,title='Title', units='A.U.'):
+    time = np.arange(0,len(signal)/fs, 1/fs)
+    plt.plot(time, signal)
+    plt.title(title)
+    plt.xlabel('time (s)')
+    plt.ylabel(units)
+    plt.grid()
+  
+def plot_filtered_data(signal, impulse_response, fs, title = 'Title', units = 'A.U.'):
+    filtered_signal = np.convolve(signal, impulse_response, mode = 'same')
+    plot_data(filtered_signal, fs, title = title, units = units )
+    return filtered_signal
+
+def find_beats(signal, fs):
+    flipped_signal = signal * -1
+    beats, info = scipy.signal.find_peaks(flipped_signal, height = 100)
+    time = np.arange(0,len(signal)/fs, 1/fs)
+    plt.scatter(time[beats], signal[beats], c = 'r')
+    return beats
+        
+    
+def hrv(beat_indicies, fs):
+    beat_indicies = np.array(beat_indicies)
+    differences = np.diff(beat_indicies/fs)
+    hrv = np.std(differences)
+    return differences, hrv
+
+def plot_frequency_bands(signal, fs, low_fc_range, high_fc_range, title = 'Title', units = 'A.U'):
+    fft = scipy.fft.rfft(signal)
+    freq = scipy.fft.rfftfreq(len(signal), fs)
+    
+    low_fc_mask = (freq >= low_fc_range[0]) & (freq <= low_fc_range[1])
+    high_fc_mask = (freq >= high_fc_range[0]) & (freq <= high_fc_range[1])
+    
+    low_fc_fft = fft[low_fc_mask]
+    low_fc = np.arange(low_fc_range[0], low_fc_range[1], ((low_fc_range[1] - low_fc_range[0]) / len(low_fc_fft)))
+    
+    high_fc_fft = fft[high_fc_mask]
+    high_fc = np.arange(high_fc_range[0], high_fc_range[1], ((high_fc_range[1] - high_fc_range[0]) / len(high_fc_fft)))
+   
+    fft_power = ((np.abs(fft)) ** 2) #/ ((len(signal) * fs))
+    
+    plt.plot(freq, fft_power, c = 'gray', zorder = 0 )
+    plt.fill_between(low_fc, np.abs(fft_power[low_fc_mask]), label = 'low frequecy band')
+    plt.fill_between(high_fc, np.abs(fft_power[high_fc_mask]), label = 'high frequency band')
+    
+    plt.title(title)
+    plt.xlabel('frequencies')
+    plt.xlim(0,high_fc_range[1])
+    plt.legend()
+    plt.grid()
+    mean_low_fc = np.mean(np.abs(low_fc_fft))
+    mean_high_fc = np.mean(np.abs(high_fc_fft))
+    ratio = mean_low_fc / mean_high_fc
+    
+    return ratio
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ project3_script.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 10:55:48 2023
+
+@author: abbyhoward
+
+project3_script
+"""
+#%% import packages
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import fft
+from scipy import signal
+import os
+import project3_module as p3m
+
+#%% part 1
+fs = 500
+
+path = 'recorded_data'
+files = os.listdir(path)
+
+data = {}
+for file in files:
+    if '.txt' in file:
+        dictionary = {file.strip('.txt'): np.loadtxt(f'{path}/{file}')}
+        data = data | dictionary
+        
+
+no_peak_data = p3m.remove_peaks(data, fs, 1000)
+
+for key,val in no_peak_data.items():
+        exec(key + '=val')
+plt.figure(1, clear = True)
+plt.title('Raw Data')
+
+plt.subplot(3,2,1)
+p3m.plot_data(relaxing_sitting,fs, 'relaxing sitting')
+plt.xlim(165,170)
+plt.ylim(-100,600)
+
+plt.subplot(3,2,2)
+p3m.plot_data(relaxing_activity, fs, 'relaxing activity')
+plt.xlim(255,260)
+plt.ylim(-100,600)
+
+plt.subplot(3,2,3)
+p3m.plot_data(mentally_stressful, fs, 'mentally stressful')
+plt.xlim(125,130)
+plt.ylim(-100,600)
+
+plt.subplot(3,2,4)
+p3m.plot_data(physically_stressful, fs, 'physically stressful')
+plt.xlim(145,150)
+plt.ylim(-100,600)
+
+plt.tight_layout()
+
+plt.subplot(3,1,3)
+concatenated_signal = np.concatenate([relaxing_sitting, relaxing_activity, mentally_stressful, physically_stressful])
+
+plt.title('Concatenated Signal')
+p3m.plot_data(concatenated_signal, fs, 'concatenated_signal')
+
+#%%
+# define filter perameters (band-stop filter)
+
+# low cutoff
+fc_low = 0.67
+#high cutoff
+fc_high = 40
+
+numtaps = 501
+
+#band stop
+impulse_response = signal.firwin(numtaps, [fc_low,fc_high], fs = fs, pass_zero = False, window = 'hann')
+
+plt.figure(3, clear = True)
+
+plt.subplot(1,2,1)
+p3m.plot_data(impulse_response, fs, 'Impulse Response')
+
+plt.subplot(1,2,2)
+fft_response = fft.rfft(impulse_response)
+f_filter = fft.rfftfreq(len(impulse_response), 1/fs)
+plt.plot(f_filter, np.abs(fft_response))
+plt.title('Frequency Domain of Filter')
+plt.xlabel('Frequencies')
+plt.grid()
+plt.tight_layout()
+
+
+plt.figure(4, clear = True)
+plt.title('Filtered Signals With Beats')
+
+plt.subplot(2,2,1)
+filt_relaxing_sitting = p3m.plot_filtered_data(relaxing_sitting, impulse_response, fs, 'filtered relaxing sitting')
+
+plt.subplot(2,2,2)
+filt_relaxing_activity = p3m.plot_filtered_data(relaxing_activity, impulse_response, fs, 'filtered relaxing activity')
+
+plt.subplot(2,2,3)
+filt_mentally_stressful = p3m.plot_filtered_data(mentally_stressful, impulse_response, fs, 'filtered mentally stressful')
+
+plt.subplot(2,2,4)
+filt_physically_stressful = p3m.plot_filtered_data(physically_stressful,impulse_response, fs, 'filtered physically stressful')
+plt.tight_layout()
+
+#%% part 3
+
+plt.figure(4)
+
+plt.subplot(2,2,1)
+relaxing_sitting_beats = p3m.find_beats(filt_relaxing_sitting, fs)
+
+plt.subplot(2,2,2)
+relaxing_activity_beats = p3m.find_beats(filt_relaxing_activity, fs)
+
+plt.subplot(2,2,3)
+mentally_stressful_beats = p3m.find_beats(filt_mentally_stressful, fs)
+
+plt.subplot(2,2,4)
+physically_stressful_beats = p3m.find_beats(filt_physically_stressful, fs)
+
+#%% part 4
+rs_ibi, rs_hrv = p3m.hrv(relaxing_sitting_beats, fs)
+
+ra_ibi, ra_hrv = p3m.hrv(relaxing_activity_beats, fs)
+
+ms_ibi, ms_hrv = p3m.hrv(mentally_stressful_beats, fs)
+
+ps_ibi, ps_hrv = p3m.hrv(physically_stressful_beats, fs)
+
+plt.figure(5, clear = True)
+plt.title('Heart Rate Veriabilities')
+
+plt.bar(['relaxing sitting', 'relaxing activity', 'mentally stressful', 'physically stressful'],[rs_hrv, ra_hrv, ms_hrv, ps_hrv])
+plt.tight_layout()
+
+
+rs_time = np.arange(0,len(relaxing_sitting) / fs, 0.1)
+rs_interp = np.interp(rs_time, relaxing_sitting_beats[1:] / fs, rs_ibi)
+
+ra_time = np.arange(0,len(relaxing_activity) / fs, 0.1)
+ra_interp = np.interp(ra_time, relaxing_activity_beats[1:] / fs, ra_ibi)
+
+ms_time = np.arange(0,len(mentally_stressful) / fs, 0.1)
+ms_interp = np.interp(ms_time, mentally_stressful_beats[1:] / fs, ms_ibi)
+
+ps_time = np.arange(0,len(physically_stressful) / fs, 0.1)
+ps_interp = np.interp(ps_time, physically_stressful_beats[1:] / fs, ps_ibi)
+
+plt.figure(6, clear = True)
+plt.title('Frequency Domain')
+
+plt.subplot(2,2,1)
+rs_ratio = p3m.plot_frequency_bands(rs_interp, 0.1, [0.04,0.15], [0.15,0.4], title = 'Relaxing Sitting')
+plt.ylim(0,5000)
+plt.subplot(2,2,2)
+ra_ratio = p3m.plot_frequency_bands(ra_interp, 0.1, [0.04,0.15], [0.15,0.4], title = 'Relaxing Activity')
+plt.ylim(0,10000)
+plt.subplot(2,2,3)
+ms_ratio = p3m.plot_frequency_bands(ms_interp, 0.1, [0.04,0.15], [0.15,0.4], title = 'Mentally Stressful')
+plt.ylim(0,5000)
+plt.subplot(2,2,4)
+ps_ratio = p3m.plot_frequency_bands(ps_interp, 0.1, [0.04,0.15], [0.15,0.4], title = 'Physically Stressful')
+plt.ylim(0,5000)
+plt.tight_layout()
+
+plt.figure(7, clear = True)
+plt.title('LF/HF Ratios')
+plt.bar(['relaxing sitting','relaxing activity','mentally stressful','physically stressful'], [rs_ratio, ra_ratio, ms_ratio, ps_ratio])
+plt.tight_layout()
+
